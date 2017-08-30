36b6a3cce25b831c3b7842657ec709cf040f789a
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2017-08-30 14:15:35 +0200

    Rename Non_used_jy_mcu_bluetooth.ino to Arduino/Non_used_jy_mcu_bluetooth.ino

---
+++ Arduino/Non_used_jy_mcu_bluetooth.ino
@@ -0,0 +1,9 @@
+/*
+// http://www.instructables.com/id/Success-Using-the-JY-MCU-linvor-Bluetooth-Module/?ALLSTEPS
+// https://github.com/rwaldron/johnny-five/wiki/Getting-Started-with-Johnny-Five-and-JY-MCU-Bluetooth-Serial-Port-Module
+// http://mcuoneclipse.com/2014/03/30/getting-bluetooth-working-with-jy-mcu-bt_board-v1-06/
+// https://yoannq.wordpress.com/2013/12/18/jy-mcu-v1-05-serial-to-bluetooth-converter-communication-with-linux/
+//
+*/
+
+
